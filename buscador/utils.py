aa6549c91a71260922d629355fbd4300cbd6680b
dpizarrow <diego.pizarro0@gmail.com>
2022-10-29 11:59:18 -0300

    edit

--- buscador/utils.py
+++ buscador/utils.py
@@ -26,7 +26,7 @@ def get_closest_matches(features):
         print(val)
         results[fname] = val
     #print(results)
-    #results = dict(sorted(results.items(), key=lambda x:x[1]))
+    results = dict(sorted(results.items(), key=lambda x:x[1]))
     top_3 = []
     for k in results:
         top_3.append(k)
