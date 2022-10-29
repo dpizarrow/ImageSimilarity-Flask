2db2b892ab3d9b37aaa83d3175bfbb91b8e88ff8
dpizarrow <diego.pizarro0@gmail.com>
2022-10-29 10:13:19 -0300

    test

--- buscador/utils.py
+++ buscador/utils.py
@@ -2,12 +2,13 @@ import os
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-featurepath = 'features'
+featurepath = '/mc4/features'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_closest_matches(features):
-    print(os.getcwd())
+    for f in os.listdir(featurepath):
+        print(f)
 
 get_closest_matches("hola")
