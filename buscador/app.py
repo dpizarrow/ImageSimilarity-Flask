32dcfd7fd7ade46bf2eb5a9d7c73ec72d1f6d508
Diego Pizarro <69170636+dpizarrow@users.noreply.github.com>
2022-11-12 17:26:17 -0300

    Remove secret key

--- buscador/app.py
+++ buscador/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 #UPLOAD_FOLDER = 'static/uploads/'
 UPLOAD_FOLDER = '/home/usuario/mc4/buscador/static/uploads/'
 app = Flask(__name__)
-app.secret_key = "secret key"
+# Secret key
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 #API_URL = 'http://127.0.0.1:5001/redcnn-mc4'
